7bec8fe1b63dcb542afd8f6084fe02ecf0a88aac
kathy156 <katherine.romero2027@frech.superate.org>
2026-08-17 10:28:50 -0600

    practica1js

---
+++ Practicas1js/js/script.js
@@ -0,0 +1,25 @@
+document.write("Hola mundo");
+
+alert("Mundo hola");
+
+console.log("Hola chavales");
+
+//Tipos de datos 
+
+let nombre="Ana";
+
+let edad=18;
+
+let estudiante=true;
+
+let ciudad;
+
+let mascota=null;
+
+//Usando typeof para identificar cada tipo
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof estudiante);
+console.log(typeof ciudad);
+console.log(typeof mascota);
